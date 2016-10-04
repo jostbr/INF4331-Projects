2177ef03be53ee42e2b7b9975606d71634aa579e
Jostein <cyclone@Ubuntu-VirtualBox.(none)>
2016-10-04 03:42:46 +0200

    Added mandelbrot_1.py for exercise 4.1

---
+++ assignment4/mandelbrot_1.py
@@ -0,0 +1,36 @@
+
+import time
+from matplotlib import pyplot as plt
+
+def mandelbrot_python(x_min, x_max, y_min, y_max, grid_size = 1000, max_escape_iter = 1000):
+    image_array = [[[0.0 for i in range(3)] for j in range(grid_size)] for k in range(grid_size)]
+    delta_x = (x_max - x_min) / (grid_size - 1.0)
+    delta_y = (y_max - y_min) / (grid_size - 1.0)
+
+    for i in range(grid_size):
+        for j in range(grid_size):
+            c_complex = complex(x_min + i * delta_x, y_min + j * delta_y)
+            z_complex = complex(0, 0)
+            current_iter = 0
+
+            while (abs(z_complex) < 2 and current_iter < max_escape_iter):
+                z_complex = z_complex ** 2 + c_complex
+                current_iter = current_iter + 1
+
+            image_array[j][i] = get_pixel_RGB(current_iter / max_escape_iter)
+
+    plt.imshow(image_array, interpolation = "gaussian", extent = [x_min, x_max, y_min, y_max])
+    plt.show()
+
+def get_pixel_RGB(norm_iter):
+    if (norm_iter == 1):
+        return [0.0, 0.0, 0.0]
+
+    else:
+        return [0.0, 10 * norm_iter, 0.0]
+
+if (__name__ == "__main__"):
+    t_0 = time.clock()
+    mandelbrot_python(-2.5, 1, -1, 1)
+    t_1 = time.clock()
+    print("Execution took {0:.2f} seconds".format(t_1 - t_0))
