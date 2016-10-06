90ec179274de503156cd401d19449f26c190803d
Jostein <cyclone@Ubuntu-VirtualBox.(none)>
2016-10-06 03:49:03 +0200

    Made performance improvements to mandelbrot_2.py

--- assignment4/mandelbrot_1.py
+++ assignment4/mandelbrot_1.py
@@ -2,7 +2,7 @@
 import time
 from matplotlib import pyplot as plt
 
-def mandelbrot_python(x_min, x_max, y_min, y_max, grid_size, max_escape_iter = 1000):
+def mandelbrot_python(x_min, x_max, y_min, y_max, width, height, max_escape_iter = 1000):
     """Function that computes the escape times (number of iterations of the sequence f_c(0),f_c(f_c(0)),..., where f_c(z) = z² + c,
     before it encounters an element of the sequence for which the absolute value (modulus) exceeds 2) for all the complex numbers
     in the xy-plane defined by the arguments supplied to the function. This implementation uses only basic Python for computation.
@@ -18,26 +18,26 @@ def mandelbrot_python(x_min, x_max, y_min, y_max, grid_size, max_escape_iter = 1
     Returns:
         escape_iter_array (array[int]): 2D-array with number of iterations before a point is recognised to be outside the M-set
     """
-    escape_iter_array = [[0 for j in range(grid_size)] for k in range(grid_size)]   # 2D-list to store escape times for gridpoints
-    delta_x = (x_max - x_min) / (grid_size - 1.0)   # Distance between gridpoints in the x-direction
-    delta_y = (y_max - y_min) / (grid_size - 1.0)   # Distance between gridpoints in the y-direction
+    escape_iter_array = [[0 for j in range(width)] for i in range(height)]   # 2D-list to store escape times for gridpoints
+    delta_x = (x_max - x_min) / (width - 1.0)       # Distance between gridpoints in the x-direction
+    delta_y = (y_max - y_min) / (height - 1.0)      # Distance between gridpoints in the y-direction
 
-    for i in range(grid_size):
-        for j in range(grid_size):
-            c_complex = complex(x_min + i * delta_x, y_min + j * delta_y)   # Current c-value is the (i, j)'th gridpoint made complex
+    for i in range(0, height):
+        for j in range(0, width):
+            c_complex = complex(x_min + j * delta_x, y_min + i * delta_y)   # Current c-value is the (i, j)'th gridpoint made complex
             z_complex = complex(0, 0)   # z initally has the value 0 + 0j before computing the sequence f(0),f(f(0)),...
             current_iter = 0            # Variable to hold the current number of iterations of the computation of the sequence
 
-            while (abs(z_complex) < 2 and current_iter < max_escape_iter):  # While sequence doesn't exceed 2 in abs-val and max esc-time not reached
+            while ((abs(z_complex) < 2.0) and (current_iter < max_escape_iter)):  # While sequence doesn't exceed 2 in abs-val and max esc-time not reached
                 z_complex = z_complex ** 2 + c_complex      # Computing the sequence mandelbrot sequence f(0),f(f(0)),...
                 current_iter = current_iter + 1             # Increment iteraton variable with one to keep track of current iteration
 
-            escape_iter_array[j][i] = current_iter      # Store escape time for gridpoint (escape time is max_escape_time for points in the M-set)
+            escape_iter_array[i][j] = current_iter      # Store escape time for gridpoint (escape time is max_escape_time for points in the M-set)
 
     return escape_iter_array    # Return escape time list and exit function
 
 
-def visualize_mandelbrot(escape_iter_array, x_min, x_max, y_min, y_max, grid_size, max_escape_iter):
+def visualize_mandelbrot(escape_iter_array, x_min, x_max, y_min, y_max, width, height, max_escape_iter):
     """Function that takes in a list/array of escape times for the mandelbrot computations and creates an image
     giving a visual representation of the mandelbrot set. Here the coloring is such that each gridpoints is given
     a RGB (list with 3 values) value that depends on the number of iterations before exceeding 2 in absolute value.
@@ -50,11 +50,12 @@ def visualize_mandelbrot(escape_iter_array, x_min, x_max, y_min, y_max, grid_siz
         grid_size (int): The number of points in the grid (both x- and y-direction)
         max_escape_iter (int): The number of iterations to be carried out before we conclude a point is in the M-set
     """
-    image_array = [[[0.0 for i in range(3)] for j in range(grid_size)] for k in range(grid_size)]   # 3D-list to store RGB values for gridpoints
-    for i in range(grid_size):
-        for j in range(grid_size):
+    image_array = [[[0.0 for k in range(3)] for j in range(width)] for i in range(height)]   # 3D-list to store RGB values for gridpoints
+    
+    for i in range(height):
+        for j in range(width):
             if (escape_iter_array[i][j] == max_escape_iter):    # If current point is inside the M-set
-                image_array[i][j] = [0.0, 0.0, 0.0]             # Give points in the M-set color black
+                image_array[i][j] = [0.0, 0.0, 0.0]             # Give points in the M-set "color" black
 
             else:                                               # If current point not inside the M-set
                 norm_iter = escape_iter_array[i][j] / max_escape_iter                   # Normalize escape time to give value in [0, 1]
@@ -65,11 +66,11 @@ def visualize_mandelbrot(escape_iter_array, x_min, x_max, y_min, y_max, grid_siz
 
 if (__name__ == "__main__"):
     x_min = -2.5; x_max = 1; y_min = -1; y_max = 1
-    grid_size = 200; max_escape_iter = 1000
+    width = 200; height = 200; grid_size = 200; max_escape_iter = 1000
 
     time_start = time.clock()
-    escape_iter_array = mandelbrot_python(x_min, x_max, y_min, y_max, grid_size, max_escape_iter)
+    escape_iter_array = mandelbrot_python(x_min, x_max, y_min, y_max, width, height, max_escape_iter)
     time_stop = time.clock()
     print("Execution took {0:.2f} seconds".format(time_stop - time_start))
 
-    visualize_mandelbrot(escape_iter_array, x_min, x_max, y_min, y_max, grid_size, max_escape_iter)
+    visualize_mandelbrot(escape_iter_array, x_min, x_max, y_min, y_max, width, height, max_escape_iter)
--- assignment4/mandelbrot_2.py
+++ assignment4/mandelbrot_2.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mandelbrot_1 import visualize_mandelbrot
 
-def mandelbrot_numpy(x_min, x_max, y_min, y_max, grid_size, max_escape_iter = 1000):
+def mandelbrot_numpy(x_min, x_max, y_min, y_max, width, height, max_escape_iter = 1000):
     """Function that computes the escape times (number of iterations of the sequence f_c(0),f_c(f_c(0)),..., where f_c(z) = z² + c,
     before it encounters an element of the sequence for which the absolute value (modulus) exceeds 2) for all the complex numbers
     in the xy-plane defined by the arguments supplied to the function. This implementation utilises NumPy-arrays and vectorization.
@@ -20,31 +20,26 @@ def mandelbrot_numpy(x_min, x_max, y_min, y_max, grid_size, max_escape_iter = 10
     Returns:
         escape_iter_array (array[int]): 2D-array with number of iterations before a point is recognised to be outside the M-set
     """
-    x, y = np.meshgrid(np.linspace(x_min, x_max, grid_size), np.linspace(y_min, y_max, grid_size))  # Create mesh-matrices x and y
-    escape_iter_array = np.zeros((grid_size, grid_size), dtype = np.int)        # 2D-array to store escape times for gridpoints
-    outside_current_iter = np.zeros((grid_size, grid_size), dtype = np.bool)    # 2D-array to be mak for gridpoints outside M-set
-    z_complex = np.zeros((grid_size, grid_size), dtype = np.complex128)         # 2D-array to store evolution of sequence at each gridpoint
-    c_complex = x + y * 1j      # Let c be the complex number made up of x- and y-coordinate as real and imaginary part respectively
-    current_iter = 1            # Variable to hold the current number of iterations of the computation of the sequence f(0),f(f(0)),...
+    x, y = np.meshgrid(np.linspace(x_min, x_max, width), np.linspace(y_min, y_max, height))  # Create mesh-matrices x and y
+    escape_iter_array = np.zeros((height, width), dtype = np.int)   # 2D-array to store escape times for gridpoints
+    below_limit = np.zeros((height, width), dtype = np.bool)        # 2D-array to be mak for gridpoints outside M-set
+    z = np.zeros((height, width), dtype = np.complex128)            # 2D-array to store evolution of sequence at each gridpoint
+    c = x + y * 1j      # Let c be the complex number made up of x- and y-coordinate as real and imaginary part respectively
 
-    while (current_iter < max_escape_iter):                     # While we haven't exceeded the maximum set escape time
-        z_complex = z_complex ** 2 + c_complex                  # Computing (vecorization) the sequence mandelbrot sequence f(0),f(f(0)),...
-        outside_current_iter = (abs(z_complex) > 2)             # 2D-mask with True for gridpoints that gives abs() > 2
-        escape_iter_array[outside_current_iter] = current_iter  # Store escape time for all gridpoints outside M-set on this iteration
-        z_complex[outside_current_iter] = 0.0                   # "Terminate" sequence for gridpoints outside M-set on this iteration
-        c_complex[outside_current_iter] = 0.0                   # "Terminate" sequence for gridpoints outside M-set on this iteration
-        current_iter = current_iter + 1                         # Increment iteraton variable with one to keep track of current iteration
+    for current_iter in range(0, max_escape_iter):
+        below_limit = (z.real * z.real + z.imag * z.imag < 4.0)    # 2D-mask with True for gridpoints that gives abs() < 2
+        escape_iter_array[below_limit] = current_iter + 1          # Update escape time for all gridpoints with still abs() < 2
+        z[below_limit] = z[below_limit] ** 2 + c[below_limit]      # Keep computing sequence only for points still abs() < 2
 
-    escape_iter_array[(escape_iter_array == 0)] = max_escape_iter   # Escape time for gridpoints within within M-set is set to max_escape_iter
-    return escape_iter_array                                        # Return escape time array and exit function
+    return escape_iter_array    # Return escape time array and exit function
 
 if (__name__ == "__main__"):
-    x_min = -2.5; x_max = 1; y_min = -1; y_max = 1;     # Set parameters
-    grid_size = 200; max_escape_iter = 1000;           # Set parameters
+    x_min = -0.6; x_max = -0.45; y_min = -0.68; y_max = -0.5;     # Set parameters
+    width = 2000; height = 2000; max_escape_iter = 5000;  # Set parameters
 
     time_start = time.clock()
-    escape_iter_array = mandelbrot_numpy(x_min, x_max, y_min, y_max, grid_size, max_escape_iter)    # Call to the pure Python version
+    escape_iter_array = mandelbrot_numpy(x_min, x_max, y_min, y_max, width, height, max_escape_iter)    # Call to the NumPy version
     time_stop = time.clock()
     print("Execution took {0:.2f} seconds".format(time_stop - time_start))
 
-    visualize_mandelbrot(escape_iter_array, x_min, x_max, y_min, y_max, grid_size, max_escape_iter) # Visualize the results using matplotlib
+    visualize_mandelbrot(escape_iter_array, x_min, x_max, y_min, y_max, width, height, max_escape_iter) # Visualize the results using matplotlib
